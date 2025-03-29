480ab68af5eb5eb55ebc679b8f82d8231f7baffc
byankatomaz <byankatomaz@gmail.com>
2025-03-29 01:00:36 -0300

    up all project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,79 +1,76 @@
 'use client'
 
-import { useRef, useState } from "react";
+import { content } from "@/interfaces/content-pages";
+import Image from "next/image";
+import { useRef } from "react";
 import HTMLFlipBook from "react-pageflip";
-
-const pages = [
-  { title: "", content: "", cover: true }, // Capa do livro
-  { title: "Nosso Início", content: "Era uma vez... o começo da nossa linda história." },
-  { title: "Primeiro Encontro", content: "O dia em que nossos olhares se cruzaram e tudo mudou." },
-  { title: "Momentos Felizes", content: "Cada risada, cada abraço, cada momento inesquecível." },
-  { title: "Nosso Futuro", content: "E essa história continua... para sempre ao seu lado." },
-  { title: "", content: "", backCover: true }, // Contra capa do livro
-];
 
 export default function Home() {
   const bookRef = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-pink-200 p-4">
-      {!isOpen ? (
-        <div
-          className="w-[300px] h-[400px] bg-pink-500 text-white font-bold text-2xl flex items-center justify-center shadow-lg cursor-pointer"
-          onClick={() => setIsOpen(true)}
-        >
-          <h1>Nosso Livro de Amor 💖</h1>
-        </div>
-      ) : (
-        <HTMLFlipBook
-          width={300}
-          height={400}
-          minWidth={250}
-          maxWidth={500}
-          minHeight={300}
-          maxHeight={600}
-          showCover={true}
-          className=""
-          ref={bookRef}
-          style={{ margin: "auto" }}
-          startPage={0}
-          size="fixed"
-          drawShadow={true}
-          flippingTime={1000}
-          usePortrait={true}
-          startZIndex={0}
-          autoSize={true}
-          maxShadowOpacity={0.5}
-          mobileScrollSupport={true}
-          clickEventForward={false}
-          useMouseEvents={true}
-          swipeDistance={2}
-          showPageCorners={true}
-          disableFlipByClick={true}
-        >
-          {pages.map((page, index) => (
+    <div className="relative flex h-screen flex-col items-center justify-center bg-pink-200 p-4">
+      <HTMLFlipBook
+        className="relative"
+        width={300}
+        height={400}
+        minWidth={250}
+        maxWidth={500}
+        minHeight={300}
+        maxHeight={600}
+        showCover={true}
+        ref={bookRef}
+        style={{ margin: "auto" }}
+        startPage={0}
+        size="fixed"
+        drawShadow={true}
+        flippingTime={1000}
+        usePortrait={true}
+        startZIndex={0}
+        autoSize={true}
+        maxShadowOpacity={0.5}
+        mobileScrollSupport={true}
+        clickEventForward={true}
+        useMouseEvents={true}
+        swipeDistance={2}
+        showPageCorners={true}
+        disableFlipByClick={false}
+      >
+        {content.map((page, index) => {
+          const isFirstPage = index === 0;
+          const isLastPage = index === content.length - 1;
+
+          return (
             <div
               key={index}
-              className={`w-full h-full flex flex-col justify-center items-center bg-white p-6 text-center ${
-                page.cover ? 'bg-pink-500 text-white font-bold text-2xl' :
-                page.backCover ? 'bg-gray-300 text-gray-700 font-bold text-2xl' : ''
+              className={`relative w-full h-full flex flex-col justify-center items-center p-6 text-center overflow-hidden bg-white
               }`}
             >
-              {page.cover ? (
-                <h1>Nosso Livro de Amor 💖</h1>
-              ) : page.backCover ? (
-                <h1>Fim 💖</h1>
-              ) : (
-                <>
-                  <h2 className="text-xl font-bold text-pink-600">{page.title}</h2>
-                  <p className="mt-4 text-gray-700">{page.content}</p>
-                </>
-              )}
+              <div className="absolute inset-0 bg-heart-pattern opacity-30 animate-float"></div>
+
+              <div className="relative z-10">
+                
+                <h2 className={`text-xl font-bold text-pink-600 font-letter ${isFirstPage || isLastPage ? "flex text-4xl justify-center items-center mt-24" : ""}`}>{page.title}</h2>
+                <p className={`mt-4 text-gray-700 text-sm font-classic ${isFirstPage || isLastPage ? "text-base" : ""}`}>{page.content}</p>
+                <div className="flex justify-center items-center mt-10">
+                  {page.imageSrc && page.imageSrc !== "" ? (
+                    <div className="w-52 h-52 relative">
+                      <Image
+                        src={`/images/${page.imageSrc}`}
+                        alt={page.title}
+                        fill
+                        objectFit="cover"
+                      />
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
             </div>
-          ))}
-        </HTMLFlipBook>
-      )}
+          );
+        })}
+      </HTMLFlipBook>
     </div>
   );
 }
